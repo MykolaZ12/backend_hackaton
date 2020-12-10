59babe8257efb143a2dfcd5d92d85d2f0c07823e
Mykola Riabchenko <mriabchenko11@gmail.com>
2020-12-10 18:46:54 +0200

    added CORS

--- config/settings.py
+++ config/settings.py
@@ -2,7 +2,19 @@ from fastapi_mail import ConnectionConfig
 
 from config import local_config
 
+PROJECT_NAME = "FastAPI - Astronomical Events"
+VERSIONS = 1.0
+
+API_V1_STR = "/api/v1"
+
 SQLALCHEMY_DATABASE_URL = "postgresql://admin:password1234@tymkiv.pp.ua:5432/postgres"
+
+SECRET_KEY = local_config.SECRET_KEY
+
+BACKEND_CORS_ORIGINS = [
+    "http://localhost",
+    "http://localhost:8080",
+]
 
 conf = ConnectionConfig(
     MAIL_USERNAME=local_config.MAIL_USERNAME,
--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from starlette.middleware.sessions import SessionMiddleware
+
 from app.astronomy.endpoints import event
 from app.contact.endpoints import contact
+from config import settings
+
+app = FastAPI(
+    title=settings.PROJECT_NAME,
+    version=settings.VERSIONS)
 
 
-app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.BACKEND_CORS_ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-app.include_router(event.router, prefix="/event", tags=["Astronomical Events"])
-app.include_router(contact.router, tags=["Send Email"])
+# app.add_middleware(SessionMiddleware, secret_key=settings.SECRET_KEY)
+
+app.include_router(event.router, prefix=settings.API_V1_STR, tags=["Astronomical Events"])
+app.include_router(contact.router,  prefix=settings.API_V1_STR,  tags=["Send Email"])
